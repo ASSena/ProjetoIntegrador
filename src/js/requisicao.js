3e46170b4d7b97afeefc15ea8b85c208e2c4bdc9
Arthur Sena <arthur.silva.sena25@gmail.com>
2024-11-29 00:32:46 -0300

    Revert "Conexão do cadastro com o backend"
    This reverts commit bfd3511e38d6c0275c009f4fdb14c8c66d52d944.

--- src/js/requisicao.js
+++ src/js/requisicao.js
@@ -1,6 +1,5 @@
 async function buscarMedicos() {
     try {
-        console.log("está sendo chamado")
         const response = await fetch("http://localhost:8080/"); // Requisição para a API
         if (response.ok) {
             const medicos = await response.json(); // Converte a resposta em JSON
@@ -60,8 +59,6 @@ function exibirMedicos(medicos) {
     }
 }
 
-buscarMedicos();
-
  // Elementos do pop-up
  const popup = document.getElementById("pop-up");
  const openPopup = document.querySelector(".botao_cadastro");
@@ -85,22 +82,22 @@ buscarMedicos();
      }
  });
 
-//  // Processar o formulário de cadastro
-//  form.addEventListener("submit", (event) => {
-//      event.preventDefault(); // Evita o recarregamento da página
+ // Processar o formulário de cadastro
+ form.addEventListener("submit", (event) => {
+     event.preventDefault(); // Evita o recarregamento da página
      
-//      // Coletar os dados do formulário
-//      const nome = document.getElementById("nome").value;
-//      const telefone = document.getElementById("telefone").value;
-//      const especialidade = document.getElementById("especialidade").value;
-//      const crm = document.getElementById("crm").value;
+     // Coletar os dados do formulário
+     const nome = document.getElementById("nome").value;
+     const telefone = document.getElementById("telefone").value;
+     const especialidade = document.getElementById("especialidade").value;
+     const crm = document.getElementById("crm").value;
 
-//      // Exemplo: Exibir os dados no console (pode substituir por uma requisição a um servidor)
-//      console.log({ nome, telefone, especialidade, crm });
+     // Exemplo: Exibir os dados no console (pode substituir por uma requisição a um servidor)
+     console.log({ nome, telefone, especialidade, crm });
 
-//      // Fechar o pop-up após o envio
-//      popup.style.display = "none";
+     // Fechar o pop-up após o envio
+     popup.style.display = "none";
 
-//      // Limpar os campos do formulário
-//      form.reset();
-//  });
+     // Limpar os campos do formulário
+     form.reset();
+ });
